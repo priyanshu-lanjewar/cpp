57ec79ae3c2b80c61a493a3f2563409df8350e05
priyanshu lanjewar <priyanshu.lanjewar@yahoo.com>
2020-12-14 21:46:54 +0530

    datatypes

---
+++ basic/datatypes.cpp
@@ -0,0 +1,20 @@
+//
+// Created by priyanshu on 14/12/20.
+//
+
+using namespace std;
+#include "iostream"
+int main(){
+    int num1 = 1;
+    float num2 = 2.0;
+    double num3 = 3.0;
+    char c = 'a';
+    string ch = "apple";
+
+    cout<<"Int : "<<num1<<endl;
+    cout<<"Float : "<<num2<<endl;
+    cout<<"Double : "<<num3<<endl;
+    cout<<"Char1 : "<<c<<endl;
+    cout<<"Char2 : "<<ch<<endl;
+
+}
